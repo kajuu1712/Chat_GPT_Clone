bb777673af543491e58eec8862f77a98e96bfa09
Kajal Pandit <kajalpandit830@gmail.com>
2025-10-12 22:11:39 +0530

    Initial Commit

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -38,7 +38,7 @@ app.get("/api/test", (req, res) => {
 });
 
 // Catch-all route to serve React frontend for any other route
-app.get("/:anyPath(*)", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
