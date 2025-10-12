3953c06dc0ae9fdd1f1826639c60f1bbd551fd22
Kajal Pandit <kajalpandit830@gmail.com>
2025-10-12 22:23:12 +0530

    Initial Commit

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -16,8 +16,9 @@ const __dirname = path.resolve();
 app.use(express.json());
 app.use(cors());
 
-// Serve React frontend
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+// Serve React frontend (Vite output)
+app.use(express.static(path.join(__dirname, "../FRONTEND/dist")));
+
 
 // Connect to MongoDB
 const connectDB = async () => {
@@ -37,9 +38,8 @@ app.get("/api/test", (req, res) => {
   res.json({ message: "Backend is running!" });
 });
 
-// Catch-all route to serve React frontend for any other route
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../FRONTEND/dist", "index.html"));
 });
 
 // Start server
